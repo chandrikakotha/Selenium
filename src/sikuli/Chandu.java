d3439b36ae032e001198b6ca275a3962e7195c33
chandrikakotha <chandrika.k15@gmail.com>
2025-08-19 08:35:53 +0530

    Modified

---
+++ src/sikuli/Chandu.java
@@ -0,0 +1,14 @@
+package sikuli;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+
+public class Chandu {
+  @Test
+  public void f() {
+  }
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+}
